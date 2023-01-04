e33c94c8bc52ef7201cd2094dc0eb0fc86dafc61
Kene <kayyungx@gmail.com>
2023-01-04 16:04:24 +0100

    Add trimmer decorator test

---
+++ spec/trim_decorator_spec.rb
@@ -0,0 +1,13 @@
+require "./trim_decorator"
+
+describe TrimmerDecorator do
+  let(:nameable) { double("nameable") }
+  let(:decorator) { TrimmerDecorator.new(nameable) }
+
+  describe "#correct_name" do
+    it "trims the correct name of the decorated object to a maximum of 10 characters" do
+      allow(nameable).to receive(:correct_name).and_return("John Smithh")
+      expect(decorator.correct_name).to eq("John Smith")
+    end
+  end
+end
